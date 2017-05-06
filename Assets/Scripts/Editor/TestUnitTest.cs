1df772e20a8014378c25a0f174576be6b788bec7
thetmkay <me@georgenishimura.com>
2017-05-06 18:17:14 +0100

    default project init + unit test

---
+++ Assets/Scripts/Editor/TestUnitTest.cs
@@ -0,0 +1,18 @@
+﻿
+using UnityEditor;
+using NUnit.Framework;
+
+public class TestUnitTest {
+
+	[Test]
+	public void SuccessfulTest()
+	{
+        Assert.IsTrue(true, "The test passes by default");
+	}
+
+    [Test]
+    public void FailingTest()
+    {
+
+    }
+}
