dc3a039f4112f5a7fd33fc6bb74337138303fc73
thetmkay <me@georgenishimura.com>
2017-05-07 17:44:25 +0100

    switch to android

--- build.sh
+++ build.sh
@@ -1,15 +1,11 @@
 #!/usr/bin/env sh
 
-brew config
-brew list
-
-
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
-  -buildTarget ios \
+  -buildTarget android \
   -runEditorTests
 
 cat unity.log
