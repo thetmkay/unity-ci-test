b6c61128b0d30759d8ff56638135d99457d6040d
thetmkay <me@georgenishimura.com>
2017-05-07 16:52:29 +0100

    clean up build script

--- build.sh
+++ build.sh
@@ -3,16 +3,13 @@
 brew config
 brew list
 
-ls /Applications
-ls /Applications/Unity
-ls /Applications/Unity/Unity.app/Contents
-ls /Applications/Unity/Unity.app/Contents/MacOS
 
 /Applications/Unity/Unity.app/Contents/MacOS/Unity \
   -batchmode \
   -nographics \
-  -silent-crashes \
   -logFile $(pwd)/unity.log \
   -projectPath $(pwd) \
   -buildTarget ios \
   -runEditorTests
+
+cat unity.log
